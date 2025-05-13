c003df8553048c918b17cb6c4df7a03ae4eb4f07
Samuel Bourgeat <samuel.kumar@hotmail.fr>
2025-05-13 17:22:23 +0200

    Refactor main application structure; move Tortoise ORM registration into create_application function and add ping API router

--- project/app/main.py
+++ project/app/main.py
@@ -5,22 +5,23 @@ import os
 from fastapi import FastAPI, Depends  
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.config import get_settings, Settings 
 
-app  = FastAPI()
+from app.api import ping 
+
+def create_application() -> FastAPI:
+    app  = FastAPI()
+
+    register_tortoise( 
+        app,
+        db_url = os.environ.get("DATABASE_URL"),
+        modules = {"models" : ["app.models.tortoise"]},
+        generate_schemas = False,
+        add_exception_handlers = True
+    )
+
+    app.include_router(ping.router)
+
+    return app 
 
 
-register_tortoise( 
-    app,
-    db_url = os.environ.get("DATABASE_URL"),
-    modules = {"models" : ["app.models.tortoise"]},
-    generate_schemas = False,
-    add_exception_handlers = True
-)
-
-@app.get("/ping")
-async def pong(settings : Settings = Depends(get_settings)):
-    return {"ping":"pong!",
-            "environment" : settings.environment,
-            "testing" : settings.testing}
-
+app = create_application()
