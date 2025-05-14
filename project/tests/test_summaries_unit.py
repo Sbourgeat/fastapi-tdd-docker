2d990bbb04f760d3fe294f7b5a6b7094a8f1e031
Samuel Bourgeat <samuel.kumar@hotmail.fr>
2025-05-14 21:37:57 +0200

    Implement summary generation and update CRUD operations; enhance tests for summaries

--- project/tests/test_summaries_unit.py
+++ project/tests/test_summaries_unit.py
@@ -1,4 +1,4 @@
-# project/tests/test_summaries.py
+# project/tests/test_summaries_unit.py
 
 import json
 from datetime import datetime
@@ -15,14 +15,14 @@ def test_create_summary(test_app_with_db, monkeypatch):
     monkeypatch.setattr(summaries, "generate_summary", mock_generate_summary)
 
     response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar/"})
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
     )
 
     assert response.status_code == 201
     assert response.json()["url"] == "https://foo.bar/"
 
 
-def test_create_summaries_invalid_json(test_app, monkeypatch):
+def test_create_summary_invalid_json(test_app, monkeypatch):
     def mock_generate_summary(summary_id, url):
         return None
 
@@ -66,27 +66,18 @@ def test_read_summary(test_app, monkeypatch):
     assert response.json() == test_data
 
 
-def test_read_summary_incorrect_id(test_app_with_db):
-    response = test_app_with_db.get("/summaries/999999/")
+def test_read_summary_incorrect_id(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.get("/summaries/999/")
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
 
-    response = test_app_with_db.get("/summaries/0/")
-    assert response.status_code == 422
-    assert response.json() == {
-        "detail": [
-            {
-                "ctx": {"gt": 0},
-                "input": "0",
-                "loc": ["path", "id"],
-                "msg": "Input should be greater than 0",
-                "type": "greater_than",
-            }
-        ]
-    }
 
-
-def test_read_all_summaries(test_app_with_db, monkeypatch):
+def test_read_all_summaries(test_app, monkeypatch):
     test_data = [
         {
             "id": 1,
@@ -107,70 +98,49 @@ def test_read_all_summaries(test_app_with_db, monkeypatch):
 
     monkeypatch.setattr(crud, "get_all", mock_get_all)
 
-    response = test_app_with_db.get("/summaries/")
+    response = test_app.get("/summaries/")
     assert response.status_code == 200
     assert response.json() == test_data
 
 
-def test_remove_summary_incorrect(test_app_with_db, monkeypatch):
-    test_data = [
-        {
+def test_remove_summary(test_app, monkeypatch):
+    async def mock_get(id):
+        return {
             "id": 1,
             "url": "https://foo.bar",
             "summary": "summary",
             "created_at": datetime.utcnow().isoformat(),
-        },
-        {
-            "id": 2,
-            "url": "https://testdrivenn.io",
-            "summary": "summary",
-            "created_at": datetime.utcnow().isoformat(),
-        },
-    ]
+        }
 
-    async def mock_get(id):
-        return test_data[0] if id == 1 else None
+    monkeypatch.setattr(crud, "get", mock_get)
 
     async def mock_delete(id):
         return id
 
-    monkeypatch.setattr(crud, "get", mock_get)
     monkeypatch.setattr(crud, "delete", mock_delete)
 
-    response = test_app_with_db.get("/summaries/1/")
-    summary_id = response.json()["id"]
-
-    response = test_app_with_db.delete(f"/summaries/{summary_id}/")
+    response = test_app.delete("/summaries/1/")
     assert response.status_code == 200
-    assert response.json() == {"id": summary_id, "url": "https://foo.bar/"}
+    assert response.json() == {"id": 1, "url": "https://foo.bar/"}
 
 
-def test_remove_summary_incorrect_id(test_app_with_db):
-    response = test_app_with_db.delete("/summaries/999/")
+def test_remove_summary_incorrect_id(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.delete("/summaries/999/")
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
 
-    response = test_app_with_db.delete("/summaries/0/")
-    assert response.status_code == 422
-    assert response.json() == {
-        "detail": [
-            {
-                "ctx": {"gt": 0},
-                "input": "0",
-                "loc": ["path", "id"],
-                "msg": "Input should be greater than 0",
-                "type": "greater_than",
-            }
-        ]
-    }
 
-
-def test_update_summary(test_app_with_db, monkeypatch):
+def test_update_summary(test_app, monkeypatch):
     test_request_payload = {"url": "https://foo.bar", "summary": "updated"}
     test_response_payload = {
         "id": 1,
         "url": "https://foo.bar",
-        "summary": "updated",
+        "summary": "summary",
         "created_at": datetime.utcnow().isoformat(),
     }
 
@@ -179,19 +149,12 @@ def test_update_summary(test_app_with_db, monkeypatch):
 
     monkeypatch.setattr(crud, "put", mock_put)
 
-    summary_id = 1
-
-    response = test_app_with_db.put(
-        f"/summaries/{summary_id}/",
+    response = test_app.put(
+        "/summaries/1/",
         data=json.dumps(test_request_payload),
     )
     assert response.status_code == 200
-
-    response_dict = response.json()
-    assert response_dict["id"] == summary_id
-    assert response_dict["url"] == "https://foo.bar"
-    assert response_dict["summary"] == "updated"
-    assert response_dict["created_at"]
+    assert response.json() == test_response_payload
 
 
 @pytest.mark.parametrize(
@@ -199,21 +162,21 @@ def test_update_summary(test_app_with_db, monkeypatch):
     [
         [
             999,
-            {"url": "https://foo.bar/", "summary": "Updated!"},
+            {"url": "https://foo.bar", "summary": "updated!"},
             404,
             "Summary not found",
         ],
         [
             0,
-            {"url": "https://foo.bar/", "summary": "Updated!"},
+            {"url": "https://foo.bar", "summary": "updated!"},
             422,
             [
                 {
-                    "ctx": {"gt": 0},
-                    "input": "0",
+                    "type": "greater_than",
                     "loc": ["path", "id"],
                     "msg": "Input should be greater than 0",
-                    "type": "greater_than",
+                    "input": "0",
+                    "ctx": {"gt": 0},
                 }
             ],
         ],
@@ -238,34 +201,36 @@ def test_update_summary(test_app_with_db, monkeypatch):
         ],
         [
             1,
-            {"url": "https://foo.bar/"},
+            {"url": "https://foo.bar"},
             422,
             [
                 {
                     "type": "missing",
                     "loc": ["body", "summary"],
                     "msg": "Field required",
-                    "input": {"url": "https://foo.bar/"},
+                    "input": {"url": "https://foo.bar"},
                 }
             ],
         ],
     ],
 )
-def test_update_summary_incorrect(
-    test_app_with_db, summary_id, payload, status_code, detail
+def test_update_summary_invalid(
+    test_app, monkeypatch, summary_id, payload, status_code, detail
 ):
-    response = test_app_with_db.put(
-        f"/summaries/{summary_id}/", data=json.dumps(payload)
-    )
+    async def mock_put(id, payload):
+        return None
+
+    monkeypatch.setattr(crud, "put", mock_put)
+
+    response = test_app.put(f"/summaries/{summary_id}/", data=json.dumps(payload))
     assert response.status_code == status_code
-    print(response.json()["detail"])
     assert response.json()["detail"] == detail
 
 
-def test_update_summary_invalid_json(test_app):
+def test_update_summary_invalid_url(test_app):
     response = test_app.put(
         "/summaries/1/",
-        data=json.dumps({"url": "invalid://url", "summary": "Updated!"}),
+        data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
     )
     assert response.status_code == 422
     assert (
